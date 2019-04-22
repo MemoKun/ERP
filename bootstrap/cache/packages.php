f8a50aaa024815da7f372d24bafd6b50e199e467
XUEJUNHAO <xuejunhao@mac-Teacher-Mac-Pro.local>
2019-04-22 12:13:25 +0800

    20190422中午第一次提交

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+  'dingo/api' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Dingo\\Api\\Provider\\LaravelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'API' => 'Dingo\\Api\\Facade\\API',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'liyu/dingo-serializer-switch' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Liyu\\Dingo\\ServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'tymon/jwt-auth' => 
+  array (
+    'aliases' => 
+    array (
+      'JWTAuth' => 'Tymon\\JWTAuth\\Facades\\JWTAuth',
+      'JWTFactory' => 'Tymon\\JWTAuth\\Facades\\JWTFactory',
+    ),
+    'providers' => 
+    array (
+      0 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
+    ),
+  ),
+);
