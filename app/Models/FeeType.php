68921fa36055123f9db6ca2c5c5b600333f659a7
JunhaoXue <m15763306650@163.com>
2019-04-29 17:30:53 +0800

    enter

--- app/Models/FeeType.php
+++ app/Models/FeeType.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Models;
@@ -28,31 +27,3 @@ class FeeType extends Model
         return $this->hasMany(afterCompensationOrder::class);
     }
 }
-=======
-<?php
-
-namespace App\Models;
-
-class FeeType extends Model
-{
-    protected $table = 'fee_types';
-
-    protected $fillable = [
-        'fee_category_id', 'name', 'code', 'is_default',
-        'status', 'remark'
-    ];
-
-    //设置类型
-    protected $casts = [
-        'is_default' => 'boolean',
-        'status' => 'boolean'
-    ];
-
-
-    public function feeCategory()
-    {
-        return $this->belongsTo(FeeCategory::class);
-    }
-
-}
->>>>>>> yylou15-master
